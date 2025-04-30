02ac535bea4a934d7039017d3315c70df94b557d
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-04-30 22:59:39 +0530

    Create string_input_from_user.java

---
+++ string_input_from_user.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Main
+{
+	public static void main(String args[])
+	{
+		System.out.print("Enter your name ");
+		Scanner name=new Scanner(System.in);
+		String a=name.nextLine();
+		System.out.println(a+" is your name ");
+	}
+}
