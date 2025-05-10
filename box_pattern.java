27710d7d7113098cc900b565d75ec2e5c77a6e58
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-05-10 23:06:00 +0530

    Create box_pattern.java

---
+++ box_pattern.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) 
+	{
+		
+		Scanner sn= new Scanner(System.in);
+		
+		System.out.print("enter ");
+		
+		int n=sn.nextInt();
+		
+		for(int i = 1; i<=n; i++)
+		{
+		    for(int j=1; j<=n; j++)
+		    {
+		        System.out.print("* ");
+		    }
+		    System.out.println(" ");
+		}
+	}
+}
