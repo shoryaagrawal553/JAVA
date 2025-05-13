18a3f1ef99a1ee41554aee75c6adca63abb7de45
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-05-14 01:05:28 +0530

    Update even_or_odd.java

--- even_or_odd.java
+++ even_or_odd.java
@@ -1,18 +1,28 @@
-import java.util.Scanner;
+import java.util.Scanner; // Importing the Scanner class for taking user input
+
+// Main class
 public class Main
 {
-	public static void main(String[] args) 
-	{
-		System.out.println("Enter a number ");
-		Scanner num= new Scanner(System.in);
-		int a=num.nextInt();
-		
-		if (a%2==0)
-		{
-		    System.out.println(a+" is an even number");
-		}
-		else{
-		    System.out.println(a+" is an odd number");
-		}
-	}
+    // Main method - the entry point of the program
+    public static void main(String[] args) 
+    {
+        // Prompting the user to enter a number
+        System.out.println("Enter a number ");
+        
+        // Creating a Scanner object to read input from the user
+        Scanner num = new Scanner(System.in);
+        
+        // Reading an integer input from the user
+        int a = num.nextInt();
+        
+        // Checking if the number is even or odd
+        if (a % 2 == 0) // If the number is divisible by 2 with no remainder, it is even
+        {
+            System.out.println(a + " is an even number"); // Printing that the number is even
+        }
+        else // If the number is not divisible by 2, it is odd
+        {
+            System.out.println(a + " is an odd number"); // Printing that the number is odd
+        }
+    }
 }
