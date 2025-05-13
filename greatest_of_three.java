b6330433a2e29c73ea63e16476023e38682d5449
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-05-14 01:07:13 +0530

    Update greatest_of_three.java

--- greatest_of_three.java
+++ greatest_of_three.java
@@ -1,26 +1,24 @@
-import java.util.Scanner;
-class Main
-{
-	public static void main(String args[])
-	{
-		Scanner sc=new Scanner(System.in);
-		System.out.print("enter three numbers ");
-		int a=sc.nextInt();
-		int b=sc.nextInt();
-		int c=sc.nextInt();
+import java.util.Scanner; // Importing the Scanner class for taking user input
 
-		if(a>b && a>c)
-		{
-			System.out.println(a+" is greatest ");
-		}
-		else if(b>c)
-		{
-			System.out.println(b+" is greatest ");
-		}
-		else
-		{
-			System.out.println(c+" is greatest ");
+// Main class
+class Main {
+    // Main method - the entry point of the program
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in); // Creating a Scanner object to read input
 
-		}
-	}
+        // Prompting the user to enter three numbers
+        System.out.print("Enter three numbers: ");
+        int a = sc.nextInt(); // Reading the first number
+        int b = sc.nextInt(); // Reading the second number
+        int c = sc.nextInt(); // Reading the third number
+
+        // Checking which number is the greatest
+        if (a > b && a > c) { // If 'a' is greater than both 'b' and 'c'
+            System.out.println(a + " is greatest"); // Output 'a' as the greatest
+        } else if (b > c) { // Otherwise, if 'b' is greater than 'c'
+            System.out.println(b + " is greatest"); // Output 'b' as the greatest
+        } else { // If neither 'a' nor 'b' is the greatest, 'c' must be the greatest
+            System.out.println(c + " is greatest"); // Output 'c' as the greatest
+        }
+    }
 }
