23aa16dd0c313069d40e8f3a74e610db8eb25dba
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-05-15 12:16:23 +0530

    Create 2SUM.java

---
+++ 2SUM.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main (String[] args) 
+    {
+        int[] arr={3,8,9,15,16};
+        int n= 5;
+        int st=0, en=n-1;
+        
+        Scanner sc= new Scanner(System.in);
+        
+        System.out.print("enter the target: ");
+        int tar= sc.nextInt();
+        
+        while(st<en)
+        {
+            int sum=arr[st]+arr[en];
+            
+            if(sum==tar){
+                System.out.print("elements found at index "+ st +", "+en);
+                break;
+            }
+            
+            else if(tar < sum )
+            {
+                en--;
+            }
+            else{
+                st++;
+            }
+        }
+        
+        System.out.print("element not found ");
+    }
+}
