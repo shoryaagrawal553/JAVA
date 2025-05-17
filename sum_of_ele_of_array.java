51c2d485cfe1f7b653958926231cc1273943f687
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-05-17 21:27:35 +0530

    Create sum_of_ele_of_array.java

---
+++ sum_of_ele_of_array.java
@@ -0,0 +1,15 @@
+class Main {
+    public static void main(String[] args) {
+
+        int []arr={1,2,8,9,11};
+        int n=5;
+        int sum=0;
+        
+        for(int i=0;i<n;i++)
+        {
+           sum+=arr[i];
+        }
+        
+        System.out.print("Sum of Array Element is: "+sum); 
+    }
+}
