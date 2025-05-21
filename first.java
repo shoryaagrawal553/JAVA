42644387084fe9de8aad3bc3d975b34d9591e2e7
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-05-21 22:21:28 +0530

    Update first.java

--- first.java
+++ first.java
@@ -4,6 +4,6 @@ public class Main
     // The main method is the entry point of the program
     public static void main(String[] args) {
         // Print "Hello World" to the console 
-        System.out.println("Hello World");
+        System.out.println("Hello World"); 
     }  
 }
