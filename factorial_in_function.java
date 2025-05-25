973ea2cf092994272b72d588f8559776f5fd4860
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-05-25 21:46:35 +0530

    Update factorial_in_function.java

--- factorial_in_function.java
+++ factorial_in_function.java
@@ -1,26 +1,30 @@
 import java.util.Scanner;
 
+// Main class
 public class Main
 {
+    // Function to calculate the factorial of a number
     static int fAct(int a)
     {
-        int fact=1;
+        int fact = 1;
         
-        for(int i=1; i<=a; i++)
+        // Loop to multiply numbers from 1 to a
+        for(int i = 1; i <= a; i++)
         {
-            fact*=i;
+            fact *= i;
         }
         
-        return fact;
+        return fact; // Return the calculated factorial
     }
     
-	public static void main(String[] args) 
-	{
-		System.out.print("Enter a number ");
-		
-		Scanner sc= new Scanner(System.in);
-		int a= sc.nextInt();
-		
-		System.out.println("The factorial of "+ a + " is "+fAct(a));
-	}
+    public static void main(String[] args) 
+    {
+        System.out.print("Enter a number "); // Prompt user for input
+        
+        Scanner sc = new Scanner(System.in); // Create a Scanner object for input
+        int a = sc.nextInt(); // Read integer input from user
+        
+        // Print the factorial result using the fAct function
+        System.out.println("The factorial of "+ a + " is " + fAct(a));
+    }
 }
