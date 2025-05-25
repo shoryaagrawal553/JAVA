1d10f0b4d40a51c24002afe10e3cffe3b88efd71
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-05-25 21:49:07 +0530

    Update greatest_using_func.java

--- greatest_using_func.java
+++ greatest_using_func.java
@@ -1,28 +1,36 @@
 import java.util.Scanner;
 
+// Main class containing the program
 public class Main 
 {
+    // Function to return the greater of two numbers
     static int max(int a, int b)
     {
-        if(a>b)
+        // If a is greater than b, return a
+        if(a > b)
         {
            return a; 
         }
-        
+        // Otherwise, return b
         else
         {
             return b;
         }
     }
     
+    // Main method: entry point of the program
     public static void main (String[] args) 
     {
+        // Prompt the user to enter two numbers
         System.out.print("enter two numbers ");
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         
-        int a= sc.nextInt();
-        int b= sc.nextInt();
+        // Read the first number
+        int a = sc.nextInt();
+        // Read the second number
+        int b = sc.nextInt();
         
-        System.out.println("The greatest number is " + max(a,b));
+        // Print the greatest number using the max function
+        System.out.println("The greatest number is " + max(a, b));
     }
 }
