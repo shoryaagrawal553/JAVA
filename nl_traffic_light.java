49e35d52ff808df1da85e4c7d456c2ddf0e6be21
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-05-31 22:18:55 +0530

    Update nl_traffic_light.java

--- nl_traffic_light.java
+++ nl_traffic_light.java
@@ -1,53 +1,56 @@
 import java.util.Scanner;
 
+// Main class
 class Main 
 {
     public static void main(String args[])
     {
+        // Prompt the user to input a color
         System.out.println("input a color ");
         
-        Scanner sc= new Scanner(System.in);
+        // Create a Scanner object to read input from the user
+        Scanner sc = new Scanner(System.in);
         
+        // Read the user's input and store it in the 'color' variable
         String color = sc.nextLine();
         
+        // Check if the input is "red"
         if(color.equals("red"))
         {
-            System.out.println("STOP");
+            System.out.println("STOP"); // Output for red light
         }    
-            
+        // Check if the input is "green"
         else if(color.equals("green"))
         {
-            System.out.println("GO");
+            System.out.println("GO"); // Output for green light
         }
-        
+        // Check if the input is "yellow"
         else if(color.equals("yellow"))
         {
-             System.out.println("START");
+            System.out.println("START"); // Output for yellow light
         }
-            
+        // If the input doesn't match any traffic light color
         else
         {
-            System.out.println("invalid input");
+            System.out.println("invalid input"); // Output for invalid input
         }    
-            
         
-        
-        // switch (color)
-        // {
-        //     case "red":
-        //     System.out.println("STOP");
-        //     break;
-            
-        //     case "yellow":
-        //     System.out.println("START");
-        //     break;   
-            
-        //     case "green":
-        //     System.out.println("GO");
-        //     break;
-            
-        //     default:
-        //     System.out.println("invalid input");
-        // }
+        // The following code is commented out. It shows how to do the same checks using a switch statement.
+        /*
+        switch (color)
+        {
+            case "red":
+                System.out.println("STOP");
+                break;
+            case "yellow":
+                System.out.println("START");
+                break;   
+            case "green":
+                System.out.println("GO");
+                break;
+            default:
+                System.out.println("invalid input");
+        }
+        */
     }
 };
