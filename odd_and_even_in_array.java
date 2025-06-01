ee7bb6a49cf4b45c7c75347397f0d9611ff58e9b
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-06-01 23:51:20 +0530

    Update odd_and_even_in_array.java

--- odd_and_even_in_array.java
+++ odd_and_even_in_array.java
@@ -1,22 +1,29 @@
+// Main class definition
 class Main
 {
     public static void main(String args[])
     {
-        int n=5;
-        int[] arr={1,2,3,4,5};
-        int eve=0, odd=0;
+        // Define the number of elements in the array
+        int n = 5;
+        // Initialize the array with 5 integers
+        int[] arr = {1, 2, 3, 4, 5};
+        // Variables to count even and odd numbers
+        int eve = 0, odd = 0;
         
-        for(int i=0; i<n; i++)
+        // Loop through each element in the array
+        for(int i = 0; i < n; i++)
         {
-            if(arr[i]%2==0)
+            // Check if the current element is even
+            if(arr[i] % 2 == 0)
             {
-                eve++;
+                eve++; // Increment even counter
             }
-            else{
-                odd++;
+            else {
+                odd++; // Increment odd counter
             }
         }
         
-        System.out.print("number of even elements are "+ eve+ " and odd is "+odd);
+        // Print out the count of even and odd elements
+        System.out.print("number of even elements are " + eve + " and odd is " + odd);
     }
 }
