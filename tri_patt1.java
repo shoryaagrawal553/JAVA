d7d5636317e58d48b3644aa74988787fc0020860
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-06-03 22:07:42 +0530

    Update tri_patt1.java

--- tri_patt1.java
+++ tri_patt1.java
@@ -3,26 +3,33 @@
 * *  
 * * *  
 * * * *    */
+// This program prints a left-aligned triangle pattern of asterisks based on user input.
 
 import java.util.Scanner;
+
 public class Main
 {
-	public static void main(String[] args) 
-	{
-		
-		Scanner sn= new Scanner(System.in);
-		
-		System.out.print("enter ");
-		
-		int n=sn.nextInt();
-		
-		for(int i = 1; i<=n; i++)
-		{
-		    for(int j=1; j<=i; j++)
-		    {
-		        System.out.print("* ");
-		    }
-		    System.out.println(" ");
-		}
-	}
+    public static void main(String[] args) 
+    {
+        // Create a Scanner object to read input from the user
+        Scanner sn = new Scanner(System.in);
+        
+        // Prompt the user to enter the number of rows for the triangle
+        System.out.print("enter ");
+        
+        // Read the integer input from the user
+        int n = sn.nextInt();
+        
+        // Outer loop for the number of rows
+        for(int i = 1; i <= n; i++)
+        {
+            // Inner loop for printing asterisks in each row
+            for(int j = 1; j <= i; j++)
+            {
+                System.out.print("* ");
+            }
+            // Move to the next line after printing a row
+            System.out.println(" ");
+        }
+    }
 }
