3ce5bfb771610968dc17b566c91c28ada8ee9c93
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-06-04 23:30:25 +0530

    Update while_factorial.java

--- while_factorial.java
+++ while_factorial.java
@@ -1,21 +1,34 @@
 import java.util.Scanner;
+
+// Main class definition
 class Main
 {
     public static void main(String args[])
     {
+        // Prompt the user to enter a number
         System.out.println("enter a number ");
+        
+        // Create a Scanner object to read input from the user
         Scanner sn = new Scanner(System.in);
         
-        int n= sn.nextInt();
+        // Read the integer input from the user
+        int n = sn.nextInt();
         
-        int fact=1;
-        int i=1;
-        while( i<=n)
+        // Initialize variables for factorial calculation
+        int fact = 1;
+        int i = 1;
+        
+        // Calculate factorial using a while loop
+        while (i <= n)
         {
-            fact*=i;
-            i++;
+            fact *= i; // Multiply fact by current value of i
+            i++;       // Increment i by 1
         }
+        
+        // Output the calculated factorial
         System.out.println(fact);
+        
+        // Close the Scanner to avoid resource leak
         sn.close();
     }
 };
