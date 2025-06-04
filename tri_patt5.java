71df71a4ee46011806ea6be2697f68e627f99844
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-06-04 23:26:12 +0530

    Update tri_patt5.java

--- tri_patt5.java
+++ tri_patt5.java
@@ -1,32 +1,41 @@
-/*Enter the Number:4
+/* 
+Example Output:
+Enter the Number:4
    1 
   2 3 
  4 5 6 
-7 8 9 10    */
+7 8 9 10    
+*/
 
 import java.util.Scanner;
+
 class Main {
     public static void main(String[] s) {
         
+        // Prompt the user to enter a number
         System.out.print("Enter the Number:");
-        Scanner sn=new Scanner(System.in);
+        Scanner sn = new Scanner(System.in);
         
-        int n= sn.nextInt();
-        int temp=1;
+        int n = sn.nextInt(); // Read the number of rows from user
+        int temp = 1;         // Initialize the starting number to print
         
-        for(int i=1; i<=n; i++)
+        // Outer loop for each row
+        for(int i = 1; i <= n; i++)
         {
-            for(int j=i; j<n; j++)
+            // Print leading spaces for triangle alignment
+            for(int j = i; j < n; j++)
             {
                 System.out.print(" ");
             }
             
-            for(int j=1; j<=i; j++)
+            // Print numbers for the current row
+            for(int j = 1; j <= i; j++)
             {
-                System.out.print(temp+" ");
-                temp++;
+                System.out.print(temp + " ");
+                temp++; // Increase the number for next print
             }
             
+            // Move to the next line after each row
             System.out.println();
         }
     }
