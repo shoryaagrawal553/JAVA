3619951356919715e1bfa663afd6fd9db09175c5
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-08-29 13:01:56 +0530

    Update binary_search.java

--- binary_search.java
+++ binary_search.java
@@ -1,6 +1,6 @@
 import java.util.Scanner; // Import the Scanner class for user input
       
-class Main {         
+class Main {          
     public static void main(String[ ]  args) { 
         Scanner sc = new Scanner(System.in); // Create a Scanner object to read input from the user 
 
