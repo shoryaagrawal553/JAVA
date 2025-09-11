d3b569575209818429ece8628ba5bc908378eeab
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-09-11 23:34:28 +0530

    Update box_patt2.java

--- box_patt2.java
+++ box_patt2.java
@@ -2,7 +2,7 @@ import java.util.Scanner; // Importing Scanner class for taking user input
   
 // Main class      
 public class Main        
-{
+{ 
     // Main method - execution starts  here
     public static void main(String[] args) 
     {
